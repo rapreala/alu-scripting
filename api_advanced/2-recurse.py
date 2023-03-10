206d1c0417fb293056a38583f61b0d4d5dbbb135
Ryan Apreala <r.apreala@alustudent.com>
2023-03-10 00:57:23 +0000

    fix

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -2,20 +2,25 @@
 """DOCS"""
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    """Prints a list containing the titles
-    of all hot articles for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
+    """function that queries the Reddit API and returns a list"""
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    resopnse = requests.get(url, headers=header, params=param)
+
+    if resopnse.status_code != 200:
         return None
-    data = response.json()
-    for child in data['data']['children']:
-        hot_list.append(child['data']['title'])
-    if data['data']['after'] is None:
-        return hot_list
-    return recurse(subreddit, hot_list, data['data']['after'])
+    else:
+        json_res = resopnse.json()
+        after = json_res.get('data').get('after')
+        has_next = \
+            json_res.get('data').get('after') is not None
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+
+        return recurse(subreddit, hot_list, after=after) \
+            if has_next else hot_list
