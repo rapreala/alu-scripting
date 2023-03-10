9d700adacb4b1e229cea4a2e214fa4354e6a46ea
Ryan Apreala <r.apreala@alustudent.com>
2023-03-10 00:19:41 +0000

    fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,7 +6,6 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
-    
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyBot/0.0.1'}
