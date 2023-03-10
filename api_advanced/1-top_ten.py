5fac75a84754c407ed835d00dcc57a7e9e3038c2
Ryan Apreala <r.apreala@alustudent.com>
2023-03-10 00:25:11 +0000

    second

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""DOCS"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
