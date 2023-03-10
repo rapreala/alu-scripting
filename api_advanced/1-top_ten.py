00d4c415afaac5e6c21559d9563d014171ceb3cf
Ryan Apreala <r.apreala@alustudent.com>
2023-03-10 00:50:38 +0000

    fix

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts 
+    """Prints the titles of the first 10 hot posts
     listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'MyBot/0.0.1'}
